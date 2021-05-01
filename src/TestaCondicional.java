6e5c8f2ed903a5fcded1f5bccdb35725d36c38e1
Douglas <douglas_souzaalves@outlook.com>
2021-05-01 13:56:26 -0300

    testando condições if e else

---
+++ src/TestaCondicional.java
@@ -0,0 +1,14 @@
+public class TestaCondicional {
+    public static void main(String[] args) {
+
+        int idade = 27;
+
+        System.out.println("--TESTANDO CONDIÇÃO IF - ELSE");
+
+        if(idade >= 18) {
+            System.out.println("Acesso liberado.");
+        } else {
+            System.out.println("Acesso negado");
+        }
+    }
+}
