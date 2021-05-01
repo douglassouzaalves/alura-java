2859bf936663fe5961ed5e9b2c0b3efe5820a617
Douglas <douglas_souzaalves@outlook.com>
2021-05-01 08:41:32 -0300

    Adicionando .gitignore

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int age = 27;
+        double weigth = 1.76;
+
+        System.out.println("Me chamo Douglas, tenho " + age + " anos, e minha altura é " + weigth);
+
+    }
+}
