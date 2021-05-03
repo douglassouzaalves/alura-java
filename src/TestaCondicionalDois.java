9844353cda7cdfa4a1cc5f22ab2eef48abe1df51
Douglas <douglas_souzaalves@outlook.com>
2021-05-03 09:52:00 -0300

    Adicionando nova classe Encadeados + correções

--- src/TestaCondicionalDois.java
+++ src/TestaCondicionalDois.java
@@ -1,15 +1,10 @@
 public class TestaCondicionalDois {
     public static void main(String[] args) {
+        int contador = 0;
 
-        int idade = 17;
-        boolean acompanhado = true;
-
-        System.out.println("--TESTANDO CONDIÇÃO--");
-
-        if(idade >= 18 || acompanhado) {
-            System.out.println("Acesso liberado.");
-        } else {
-            System.out.println("Acesso negado");
+        while(contador <= 20) {
+            System.out.println(contador);
+            contador++;
         }
     }
 }
